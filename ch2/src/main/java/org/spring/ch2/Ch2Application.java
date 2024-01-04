7e736fe571f0ecf07a195347a3e4c2b0faa3b2cc
윤승재 <sjyun011278@gmail.com>
2024-01-04 21:22:52 +0900

    [2024/01/04] ch2 Project 세팅

---
+++ ch2/src/main/java/org/spring/ch2/Ch2Application.java
@@ -0,0 +1,12 @@
+package org.spring.ch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ch2Application.class, args);
+  }
+}
---
+++ ch2/src/test/java/org/spring/ch2/Ch2ApplicationTests.java
@@ -0,0 +1,6 @@
+package org.spring.ch2;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch2ApplicationTests {}
